03690540cde376c13415aa918eb9c1c24184febe
Danishan <danishan089@gmail.com>
2024-03-05 16:33:07 +0530

    Updating components to Github

--- android/app/src/main/java/com/meal/MainActivity.kt
+++ android/app/src/main/java/com/meal/MainActivity.kt
@@ -15,12 +15,12 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "Meal"
 
   class MainActivity: ReactActivity() {
-    // ...
-    override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(null)
-    }
-    // ...
+  // ...
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
   }
+  // ...
+}
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
